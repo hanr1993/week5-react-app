1fc17922268bef98b11caaf5c66c00c57aa58c73
hanr1993 <92591105+hanr1993@users.noreply.github.com>
2022-05-14 10:04:45 +0100

    fixed date for 1 day forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,16 +2,25 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div className="WeatherForecastDay">
-      <div className="WeatherForecast-day">{props.data.dt}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={22} />
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-max">
-          {Math.round(props.temp.max)}°
+          {Math.round(props.data.temp.max)}°
         </span>
         <span className="WeatherForecast-min">
-          {Math.round(props.temp.min)}°
+          {Math.round(props.data.temp.min)}°
         </span>
       </div>
     </div>
